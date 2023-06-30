5685c4fe3c0806e33a8ebe3020b502cb724a0f07
Andre Silva <andre.silva@appmax.org>
2023-06-30 13:59:49 -0300

    Implementing method count by recipient on the controller

--- src/infra/http/controllers/notification.controller.ts
+++ src/infra/http/controllers/notification.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Param, Body, Post, Patch } from '@nestjs/common';
+import { Controller, Param, Body, Get, Post, Patch } from '@nestjs/common';
 import { SendNotificationUseCase } from '@core/use-cases/send-notification.use-case';
 import { ReadNotificationUseCase } from '@core/use-cases/read-notification.use-case';
 import { UnreadNotificationUseCase } from '@core/use-cases/unread-notification.use-case';
 import { CancelNotificationUseCase } from '@core/use-cases/cancel-notification.use-case';
+import { CountRecipientNotificationUseCase } from '@core/use-cases/count-recipient-notification.use-case';
 import { CreateNotificationDto } from '@infra/http/dtos/create-notification.dto';
 import { NotificationViewModel } from '../view-models/notification.view-model';
+import { NotificationCountByRecipientViewModel } from '../view-models/notification-count-by-recipient.view-model';
 
 @Controller('notifications')
 export class NotificationController {
@@ -13,6 +15,7 @@ export class NotificationController {
     private readNotificationUseCase: ReadNotificationUseCase,
     private unreadNotificationUseCase: UnreadNotificationUseCase,
     private cancelNotificationUseCase: CancelNotificationUseCase,
+    private countRecipientNotificationUseCase: CountRecipientNotificationUseCase,
   ) {}
 
   @Post()
@@ -41,5 +44,16 @@ export class NotificationController {
   @Patch(':id/cancel')
   async cancel(@Param('id') id: string) {
     await this.cancelNotificationUseCase.execute({ id });
+  }
+
+  @Get('recipients/:recipientId/count')
+  async countByRecipientId(
+    @Param('recipientId') recipientId: string,
+  ): Promise<NotificationCountByRecipientViewModel> {
+    const { count } = await this.countRecipientNotificationUseCase.execute({
+      recipientId,
+    });
+
+    return { count };
   }
 }
