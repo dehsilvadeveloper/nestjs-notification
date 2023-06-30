4a329234314ba2c7f42d2bd0944204dee40852e4
Andre Silva <andre.silva@appmax.org>
2023-06-30 12:12:21 -0300

    Implementing method unread on the controller

--- src/infra/http/controllers/notification.controller.ts
+++ src/infra/http/controllers/notification.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Param, Body, Post, Patch } from '@nestjs/common';
 import { SendNotificationUseCase } from '@core/use-cases/send-notification.use-case';
 import { ReadNotificationUseCase } from '@core/use-cases/read-notification.use-case';
+import { UnreadNotificationUseCase } from '@core/use-cases/unread-notification.use-case';
 import { CancelNotificationUseCase } from '@core/use-cases/cancel-notification.use-case';
 import { CreateNotificationDto } from '@infra/http/dtos/create-notification.dto';
 import { NotificationViewModel } from '../view-models/notification.view-model';
@@ -10,6 +11,7 @@ export class NotificationController {
   constructor(
     private sendNotificationUseCase: SendNotificationUseCase,
     private readNotificationUseCase: ReadNotificationUseCase,
+    private unreadNotificationUseCase: UnreadNotificationUseCase,
     private cancelNotificationUseCase: CancelNotificationUseCase,
   ) {}
 
@@ -29,6 +31,11 @@ export class NotificationController {
   @Patch(':id/read')
   async read(@Param('id') id: string) {
     await this.readNotificationUseCase.execute({ id });
+  }
+
+  @Patch(':id/unread')
+  async unread(@Param('id') id: string) {
+    await this.unreadNotificationUseCase.execute({ id });
   }
 
   @Patch(':id/cancel')
