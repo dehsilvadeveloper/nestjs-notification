a6b92b2da18926bba334fbd9d59de9968c01140c
Andre Silva <andre.silva@appmax.org>
2023-07-09 10:07:49 -0300

    Including env variable APP_INTERNAL_PORT

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix(process.env.API_NAME);
+  app.setGlobalPrefix(process.env.API_NAME ?? 'api');
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: process.env.API_DEFAULT_VERSION,
   });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.APP_INTERNAL_PORT ? parseInt(process.env.APP_INTERNAL_PORT) : 3000);
 }
 bootstrap();
