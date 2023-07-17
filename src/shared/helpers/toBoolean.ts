4604d266a805c913d8333d23616899c8195f7a32
Andre Silva <andre.silva@appmax.org>
2023-07-17 14:21:30 -0300

    Using method toBoolean on the config file AppConfig

--- src/shared/helpers/toBoolean.ts
+++ src/shared/helpers/toBoolean.ts
@@ -1,9 +1,5 @@
 export const toBoolean = (value: string | number | boolean): boolean => {
   const truthyValues: (string | number | boolean)[] = [true, 'true', 'True', 'TRUE', '1', 1];
 
-  /*if (value === undefined) {
-    return false;
-  }*/
-
   return truthyValues.includes(value);
 };
