19d1c27526674a038762139f893a60b567c94862
dehsilva <andre.silva.clinica@gmail.com>
2023-08-20 12:59:40 -0300

    Implementing use of dotenv config on the main.ts file

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import { config } from 'dotenv';
+// Filling environment variables
+config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
@@ -6,19 +9,28 @@ import { apiConfig } from './infra/config/api.config';
 import { corsConfig } from './infra/config/cors.config';
 
 async function bootstrap() {
+  // Create applications instance
   const app = await NestFactory.create(AppModule);
 
+  // API prefix and versioning
   app.setGlobalPrefix(apiConfig.globalPrefix);
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: apiConfig.defaultVersion,
   });
+
+  // Validation pipeline
   app.useGlobalPipes(new ValidationPipe());
 
+  // Enable shutdown hook explicitly
+  app.enableShutdownHooks();
+
+  // Cors
   if (corsConfig.enabled) {
     app.enableCors();
   }
 
+  // Application port
   await app.listen(appConfig.internalPort);
 }
 
